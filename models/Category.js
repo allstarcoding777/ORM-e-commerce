de14904a0af4a8d605ef82fa6a25d37c496e6cbd
Alexandria Ellis <allstarcoding777@outlook.com>
2023-04-03 22:08:00 -0500

    seeds info

--- models/Category.js
+++ models/Category.js
@@ -5,20 +5,27 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
-  {
-    // define columns
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
+    {
+      // define columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // define category name column
+        category_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'category',
+    }
 );
 
 module.exports = Category;
